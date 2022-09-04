12f262fa0aa305d3f2c9779cae6aeb98bb7af4a9
raiHan-N <88706029+raiHan-N@users.noreply.github.com>
2022-09-04 21:37:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kqydwk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kqydwk)
---
+++ script.js
@@ -0,0 +1,74 @@
+let floating_btn = document.getElementById('floating_btn');
+let modal = document.getElementById('modal');
+let modal_bg = document.getElementById('modal_bg');
+let addlist_form = document.getElementById('addlist_form');
+let root = document.getElementById('root');
+
+let list_product = [];
+
+floating_btn.addEventListener('click', () => {
+  if (modal.style.display == 'none') {
+    showModal();
+    return;
+  }
+
+  hideModal();
+});
+
+modal_bg.addEventListener('click', () => {
+  hideModal();
+});
+
+addlist_form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let nameProduct = event.target.product.value;
+  let priceProduct = event.target.price.value;
+
+  if (priceProduct === '' || nameProduct === '') {
+    alert('Isi Dulu Boy...');
+    return;
+  }
+
+  list_product.push({
+    name: nameProduct,
+    price: priceProduct,
+    time: new Date().toLocaleTimeString(),
+  });
+
+  event.target.product.value = '';
+  event.target.price.value = '';
+
+  renderToHtml();
+  hideModal();
+});
+
+function showModal() {
+  modal.style.display = 'flex';
+  floating_btn.style.backgroundColor = '#C4D7E0';
+  floating_btn.style.transform = 'rotate(-45deg)';
+}
+
+function hideModal() {
+  modal.style.display = 'none';
+  floating_btn.style.backgroundColor = '#56688f';
+  floating_btn.style.transform = 'rotate(0deg)';
+}
+
+function renderToHtml() {
+  root.innerHTML = '';
+
+  list_product.forEach((e, i) => {
+    root.innerHTML += `
+    <div class="card">
+      <p>${e.time}</p>
+      <div>${e.name} <span>Rp.${e.price},00</span></div>
+      <button onclick="handleDelete(${i})">Done</button>
+    </div>`;
+  });
+}
+
+function handleDelete(index) {
+  list_product.splice(index, 1);
+  renderToHtml();
+}
